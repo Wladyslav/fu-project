c2220670f3dd3384c06895e8abe662b259c44d6d
wladyslaw <wladyslawsahaidak@gmail.com>
2020-12-10 19:07:44 +0100

    add seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import Layout from "../components/layout"
 import MainTitle from "../components/MainTitle"
 import Hero from "../components/Hero"
 import TeamSection from "../components/TeamSection"
-// import SEO from '../components/SEO'
+import SEO from "../components/SEO"
 
 const IndexPage = () => (
   <Layout>
-    {/* <SEO title="Strona główna"/> */}
+    <SEO title="Strona główna"/>
     <MainTitle title="instytut praw migrantów" mainPage />
     <Hero />
     <TeamSection />
